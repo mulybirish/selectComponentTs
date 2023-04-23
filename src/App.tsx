b3c776fbb1f73e9b83aa9b41f90f12b986a8b226
mulybirish <mulugata13@gmail.com>
2023-04-23 09:33:43 +0200

    fully functional

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ const options = [
   { lable: "third", value: 3 },
   { lable: "forth", value: 4 },
   { lable: "fifth", value: 5 },
+  { lable: "first", value: 1 },
+  { lable: "second", value: 2 },
+  { lable: "third", value: 3 },
+  { lable: "forth", value: 4 },
+  { lable: "fifth", value: 5 },
 ];
 function App() {
   const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
